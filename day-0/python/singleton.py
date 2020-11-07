bb44d9e448a8c2f2d8429fad954bafd527d59613
Velmurugan Kuberan <vkuberan@gmail.com>
2020-11-07 16:10:02 +0530

    Singleton class functionality is added.

--- day-0/python/singleton.py
+++ day-0/python/singleton.py
@@ -1,3 +1,26 @@
-#Singleton Class in Python
+class Singleton:
+    __instance = None
 
-# This is Singleton Class
+    @staticmethod
+    def getInstance():
+        # Static access method.
+        if Singleton.__instance == None:
+            Singleton()
+        return Singleton.__instance
+
+    def __init__(self):
+        if Singleton.__instance != None:
+            raise Exception("This class is a Singleton!")
+        else:
+            Singleton.__instance = self
+
+
+s = Singleton()
+print(s)
+
+# This will raise an Error
+# s2 = Singleton()
+# print(s2)
+
+s2 = Singleton.getInstance()
+print(s2)
