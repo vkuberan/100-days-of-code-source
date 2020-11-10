b3c12c4e0ee1341b9aa37e8b7c7da19a6a0c1cfb
Velmurugan Kuberan <vkuberan@gmail.com>
2020-11-10 21:44:21 +0530

    Change string case to lowercase if a string contains equal (or greater) lowercase letters or to upper case

--- day-3/python/fix-string.py
+++ day-3/python/fix-string.py
@@ -1,3 +1,4 @@
+# https://www.codewars.com/kata/5b180e9fedaa564a7000009a/python
 # Fix string case
 # given a string that may have mixed uppercase and lowercase letters and your
 # task is to convert that string to either lowercase only or uppercase only
@@ -7,7 +8,7 @@
 # if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
 
 
-def solve(s):
+def fix_string_case(s):
     lower_count = sum(map(str.islower, s))
     upper_count = sum(map(str.isupper, s))
     if lower_count >= upper_count:
@@ -17,4 +18,4 @@ def solve(s):
 
 
 samplestring = input("Enter any string: ").strip()
-print(solve(samplestring))
+print(fix_string_case(samplestring))
