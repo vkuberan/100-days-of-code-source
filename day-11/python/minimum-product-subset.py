87dc727d4d2fbc7a82757e0a52e6578de956e346
Velmurugan Kuberan <vkuberan@gmail.com>
2020-11-23 16:49:24 +0530

    find minimum product possible with the subset of elements present in the array

---
+++ day-11/python/minimum-product-subset.py
@@ -0,0 +1,49 @@
+# Given an array a, we have to find minimum product possible with the subset of
+# elements present in the array. The minimum product can be single element also
+
+
+def min_product_subset(a, n):
+
+    if (n == 1):
+        return a[0]
+
+    max_neg = float('-inf')
+    min_pos = float('inf')
+    count_neg = 0
+    count_zero = 0
+    prod = 1
+
+    for i in range(0, n):
+
+        if (a[i] == 0):
+            count_zero = count_zero + 1
+            continue
+
+        if (a[i] < 0):
+            count_neg = count_neg + 1
+            max_neg = max(max_neg, a[i])
+
+        if (a[i] > 0):
+            min_pos = min(min_pos, a[i])
+
+        prod = prod * a[i]
+
+    if (count_zero == n or (count_neg == 0
+                            and count_zero > 0)):
+        return 0
+
+    if (count_neg == 0):
+        return min_pos
+
+    if ((count_neg & 1) == 0 and
+            count_neg != 0):
+
+        prod = int(prod / max_neg)
+
+    return prod
+
+
+tempArrays = input("Enter numbers with spaces: ").strip().split()
+prodArrays = [int(i) for i in tempArrays]
+lenPA = len(prodArrays)
+print(min_product_subset(prodArrays, lenPA))
