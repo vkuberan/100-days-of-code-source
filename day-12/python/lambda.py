9de4a711f326f301cd6ae1d3ebcb8ca61dc6ff45
Velmurugan Kuberan <vkuberan@gmail.com>
2020-11-30 20:56:25 +0530

    Python lambdas are little, anonymous functions, subject to a more restrictive but more concise syntax than regular Python functions.

--- day-12/python/lambda.py
+++ day-12/python/lambda.py
@@ -1,3 +1,23 @@
 # Refer https://realpython.com/python-lambda/
 # Python lambdas are little, anonymous functions, subject to a more restrictive
 # but more concise syntax than regular Python functions.
+
+# Identity Function
+
+
+def identity(x):
+    return x
+
+
+# same can be written in lambda function
+identityLambda = (lambda x: x)
+
+# Both print 10
+print(identityLambda(10))
+print(identity(10))
+
+# Lambda's can take multiple arguments
+fullName = (lambda title, fname,
+            lname: f'{title.title()}. {fname.title()} {lname.title()}')
+
+print(fullName('Mr', 'Velmurugan', 'Kuberan'))
