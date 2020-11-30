567af2caf181257add267f484213f8af73d006e4
Velmurugan Kuberan <vkuberan@gmail.com>
2020-11-30 16:16:50 +0530

    Create a NumPy array using standard python list or tuple

---
+++ day-12/python/standard-list-n-tuples.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+fromList_to_1d_Array = np.array([10, 20,  30, 40, 50])
+fromTuple_to_1d_Array = np.array((1, 2, 3, 4, 5))
+
+print(type(fromList_to_1d_Array))
+print(fromTuple_to_1d_Array)
