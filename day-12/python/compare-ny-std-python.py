b530c3613a8a3041efe94c634230a8f463516928
Velmurugan Kuberan <vkuberan@gmail.com>
2020-11-30 20:32:31 +0530

    Day 13

--- day-12/python/compare-ny-std-python.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-num_lst1 = [i for i in range(1, 10)]
-num_lst2 = [i for i in range(10, 20)]
-
-sum_list = list(map(lambda x, y: (x + y), num_lst1, num_lst2))
-
-print(sum_list)
--- day-12/python/numpy-fundamentals.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-# np is an alias, you may use any other alias, though np is 
-# strongly recommended
