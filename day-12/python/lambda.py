32648228f4fd6300ddad83d8c905214fd64096bd
Velmurugan Kuberan <vkuberan@gmail.com>
2020-11-30 20:39:41 +0530

    Python lambdas are little, anonymous functions, subject to a more restrictive but more concise syntax than regular Python functions.

--- day-12/python/lambda.py
+++ day-12/python/lambda.py
@@ -1,2 +1,3 @@
+# Refer https://realpython.com/python-lambda/
 # Python lambdas are little, anonymous functions, subject to a more restrictive
 # but more concise syntax than regular Python functions.
