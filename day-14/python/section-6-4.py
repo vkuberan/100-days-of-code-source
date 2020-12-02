e270cd034d8ddae731eeb139e65c6c873a5ee4e2
Velmurugan Kuberan <vkuberan@gmail.com>
2020-12-02 22:47:17 +0530

    Section 6 - Comparison of NumPy with Standard Python

--- day-14/python/section-6-4.py
+++ day-14/python/section-6-4.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import random
 
 # How to initialize np arrays when size is known
 # np.arange()
@@ -7,17 +8,28 @@ decrement = np.arange(20, 1, -3)
 
 # np.zeros()
 zeros = np.zeros(5)
-multizeros = np.zeros((3, 3, 5))
+multizeros = np.zeros((2, 2, 4))
 
 # np.ones()
-# np.random.random()
+ones = np.ones(5)
+multiones = np.ones((2, 2, 4))
+
+# np.random.random(), generate random array
+random = np.random.random([3, 3])
+
 # np.random.randint()
+randint = np.random.randint(1, 100, (3, 3))
+
 # np.inspace()
 # np.full()
 # np.eye()
 # np.title()
 
 print("ARange Increment: {}".format(increment))
-print("ARange Decrement: {}".format(decrement))
-print("Zeros: \n{}".format(zeros))
-print("Multidimentional Zeros: \n{}".format(multizeros))
+print("\n\nARange Decrement: {}".format(decrement))
+print("\n\nZeros: \n{}".format(zeros))
+print("\n\nMultidimentional Zeros: \n{}".format(multizeros))
+print("\n\nOnes: \n{}".format(ones))
+print("\n\nMultidimentional Ones: \n{}".format(multiones))
+print("\n\nRandom Array: \n{}".format(random))
+print("\n\nRandom Array Int: \n{}".format(randint))
