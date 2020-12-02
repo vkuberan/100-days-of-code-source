795055dba82bb723564e020f45b763be1c7da01c
Velmurugan Kuberan <vkuberan@gmail.com>
2020-12-02 20:29:50 +0530

    Section 6 - Comparison of NumPy with Standard Python

--- day-14/python/section-6-1.py
+++ day-14/python/section-6-1.py
@@ -2,12 +2,15 @@ import numpy as np
 
 # Compare NumPy with Standard Python Lists
 list_1 = [i for i in range(1, 20, 2)]
-list_2 = [i for i in range(1, 11, 1)]
+list_2 = [i for i in range(1, 20, 1)]
 
+# If you add different size list it will add in normal Python
 sum_list = list(map(lambda x, y: x + y, list_1, list_2))
 print(list_1)
 print(list_2)
 print(sum_list)
 
-np_sum = np.array(list_1) + np.array(list_2)
+# If you had different list sizes NumPy will trigger an error
+# np_sum = np.array(list_1) + np.array(list_2)
+np_sum = np.array(list_1) + np.array(list_2[:10])
 print(np_sum)
