4ff7ce542bc0e59172c4e6e767ace7d491046635
Velmurugan Kuberan <vkuberan@gmail.com>
2020-12-03 21:58:07 +0530

    Section 7 and 8 - Structure of Arrays, Indexing, Subsetting, Slicing and Iterating through Array

---
+++ day-15/python/section-7-structure-of-array.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Section 7: Structure of Arrays
+# dtype: finds data type of array.
+# shape: shows shape of the array(n x m).
+# itemsize: Memory used by each array element in bytes.
+# ndim: Number of axes(of dimensions).
+
+structuresOfArray = np.ones((5, 3))
+print("Sample Array: \n{}\n\n\n".format(structuresOfArray))
+print("Data Type: {}".format(structuresOfArray.dtype))
+print("Item Size: {}".format(structuresOfArray.itemsize))
+print("N Dimention: {}".format(structuresOfArray.ndim))
+print("Array Shape: {}".format(structuresOfArray.shape))
