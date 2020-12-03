cff0a1ca574a68ae59651de66fa9e58fe3946d2e
Velmurugan Kuberan <vkuberan@gmail.com>
2020-12-03 23:07:22 +0530

    Section 7 and 8 - Structure of Arrays, Indexing, Subsetting, Slicing and Iterating through Array

--- day-15/python/section-8-index-subset-slice-iterate-2d.py
+++ day-15/python/section-8-index-subset-slice-iterate-2d.py
@@ -11,3 +11,6 @@ print("Get Entire Row using index array_2d[1] or array_2d[1, :]: {}".format(
     array_2d[1]))
 print("Get Entire Column using index array_2d[:, 1]: {}".format(
     array_2d[:, 1]))
+print("Differnt Access 1 array_2d[:2, :]: {}".format(array_2d[:2, :]))
+print("Differnt Access 2 array_2d[:2, 1:5]: {}".format(array_2d[:2, 1:5]))
+print("Differnt Access 3 array_2d[:, 3:8]: {}".format(array_2d[:, 3:8]))
