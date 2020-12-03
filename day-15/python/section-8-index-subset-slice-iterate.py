8a72851fa4f9724a1e2233fdbc60bec983d9bf34
Velmurugan Kuberan <vkuberan@gmail.com>
2020-12-03 22:18:47 +0530

    Section 7 and 8 - Structure of Arrays, Indexing, Subsetting, Slicing and Iterating through Array

---
+++ day-15/python/section-8-index-subset-slice-iterate.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Section 8
+# Indexing, Subsetting, Slicing and Iterating through Arrays
+
+array_1d = np.arange(10)
+print("Array: \n{}\n".format(array_1d))
+print("Get Third Element(index) of the Array: {}".format(array_1d[2]))
+print(
+    "Get Section of (start index - end index) of the Array: {}".format(array_1d[2:5]))
