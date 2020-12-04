32cf36be8c8ea0c3f0d5ea8dddf6c8a0f6dd8b16
Velmurugan Kuberan <vkuberan@gmail.com>
2020-12-04 19:37:45 +0530

    Section 7 and 8 - Structure of Arrays, Indexing, Subsetting, Slicing and Iterating through Array

--- day-16/python/section-9-execution-speed.py
+++ day-16/python/section-9-execution-speed.py
@@ -3,12 +3,13 @@ import time
 
 # Section 9 - Execution speed of NumPy compared with Stanadard Python
 print("Standard Python")
-list1 = [i for i in range(20000000)]
-list2 = [i for i in range(20000000, 40000000)]
 
 startExecution = time.time()
+list1 = [i for i in range(200000)]
+list2 = [i for i in range(200000, 400000)]
 perform_Ops = list(map(lambda x, y: (x * y), list1, list2))
 endExecution = time.time()
+
 timeTakenForExecution = (endExecution - startExecution)
 
 
@@ -17,12 +18,12 @@ print("Total time taken for Standard List Multiplication: {}".format(
 
 print("\nNumPy")
 
-list1 = np.array([i for i in range(20000000)], dtype=np.int64)
-list2 = np.array([i for i in range(20000000, 40000000)], dtype=np.int64)
-
 startExecution = time.time()
+list1 = np.array([i for i in range(200000)], dtype=np.int64)
+list2 = np.array([i for i in range(200000, 400000)], dtype=np.int64)
 perform_Ops = list1 * list2
 endExecution = time.time()
+
 timeTakenForExecution = (endExecution - startExecution)
 
 
