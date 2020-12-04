05d3520619a59db23f10dbda979c6c98c18c4dbf
Velmurugan Kuberan <vkuberan@gmail.com>
2020-12-04 19:15:35 +0530

    NumPy Execution speed, File Handling and Operations

--- day-16/python/section-9-execution-speed.py
+++ day-16/python/section-9-execution-speed.py
@@ -2,3 +2,29 @@ import numpy as np
 import time
 
 # Section 9 - Execution speed of NumPy compared with Stanadard Python
+print("Standard Python")
+list1 = [i for i in range(20000000)]
+list2 = [i for i in range(20000000, 40000000)]
+
+startExecution = time.time()
+perform_Ops = list(map(lambda x, y: (x * y), list1, list2))
+endExecution = time.time()
+timeTakenForExecution = (endExecution - startExecution)
+
+
+print("Total time taken for Standard List Multiplication: {}".format(
+    timeTakenForExecution))
+
+print("\nNumPy")
+
+list1 = np.array([i for i in range(20000000)], dtype=np.int64)
+list2 = np.array([i for i in range(20000000, 40000000)], dtype=np.int64)
+
+startExecution = time.time()
+perform_Ops = list1 * list2
+endExecution = time.time()
+timeTakenForExecution = (endExecution - startExecution)
+
+
+print("Total time taken for NumPy Multiplication: {}".format(
+    timeTakenForExecution))
