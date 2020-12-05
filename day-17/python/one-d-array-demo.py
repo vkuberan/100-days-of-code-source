7152a416fc69d083690ac425d8cc65995feff84b
Velmurugan Kuberan <vkuberan@gmail.com>
2020-12-05 11:37:03 +0530

    Small Program to demostrate the whole NumPy(1-D Array) functionality

--- day-17/python/one-d-array-demo.py
+++ day-17/python/one-d-array-demo.py
@@ -6,8 +6,19 @@ print("Welcome to NumPy 'Single Dimentional Array' Demo")
 continueProgram = "Y"
 
 while continueProgram == "Y":
-    print("Enter List Data")
+    try:
+        min, max = input(
+            "Enter Minimum, Maximum value for the list Ex: (10, 20): ").strip().split(",")
+        if min <= 0 or max <= 0:
+            min, max = 1, 10
+        if min >= max:
+            min, max = (max - 1), min
+    except:
+        min, max = 1, 10
+
+    print("Min: {}, Max: {}".format(min, max))
+
     continueProgram = input(
-        "Y(es) to continue or any key to terminate...").trim().upper()
+        "Y(es) to continue or any key to terminate... ").strip().upper()
 
 print("End of Program.")
