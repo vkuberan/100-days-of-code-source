b169cc03a937101586ef0179ddcff81047e42f4c
Velmurugan Kuberan <vkuberan@gmail.com>
2020-12-05 11:26:12 +0530

    Small Program to demostrate the whole NumPy(1-D Array) functionality

---
+++ day-17/python/one-d-array-demo.py
@@ -0,0 +1,13 @@
+import numpy as np
+import time
+
+print("Welcome to NumPy 'Single Dimentional Array' Demo")
+
+continueProgram = "Y"
+
+while continueProgram == "Y":
+    print("Enter List Data")
+    continueProgram = input(
+        "Y(es) to continue or any key to terminate...").trim().upper()
+
+print("End of Program.")
