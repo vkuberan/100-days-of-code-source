d09292a1fe9dede06ea0a5e976826b0044642285
Velmurugan Kuberan <vkuberan@gmail.com>
2020-12-14 15:55:49 +0530

    Formatting Strings

---
+++ day-20/python/helper.py
@@ -0,0 +1,177 @@
+import numpy as np
+import time
+import platform
+import subprocess
+
+
+def clear_screen():
+    command = "cls" if platform.system().lower() == "windows" else "clear"
+    return subprocess.call(command, shell=True)
+
+
+def view_1d_info(arrayProperties):
+    msg = "View All Array Properties."
+    print(msg)
+    print("*" * len(msg))
+    print("Array Size: {}".format(arrayProperties['listSize']))
+    # print("Array Value: {}".format(oneDimPythonList))
+    print("Array Data Type: {}".format(
+        "Float" if arrayProperties['listType'] == 'F' else "Integer"))
+    print("Sequential or Random: {}".format(
+        "Random" if arrayProperties['seqOrRandom'] == 'R' else "Sequential"))
+    if arrayProperties['seqOrRandom'] == 'R':
+        print("Random Minimum: {}, Random Maximum: {}".format(
+            arrayProperties['ifRandom'][0], arrayProperties['ifRandom'][1]))
+
+
+def view_1d_array(info):
+    msg = "View Array Values."
+    print(msg)
+    print("*" * len(msg))
+
+    strList = "First List: [{}, . . . , {}]\n".format(', '.join(map(
+        str, info['listData'][0][0:5])), ', '.join(map(str, info['listData'][0][-5:])))
+    strList += "Second List: [{}, . . . , {}]".format(', '.join(map(
+        str, info['listData'][1][0:5])), ', '.join(map(str, info['listData'][1][-5:])))
+
+    print(strList)
+
+
+# Going to refactor these 4 functions in 1.
+def perform_operations(info, action='add'):
+
+    op = ''
+    retValue = ''
+
+    if action == 'add':
+        op = "Addition"
+        retValue = perform_add_operation(info)
+    elif action == 'sub':
+        op = "Subtraction"
+        retValue = perform_sub_operation(info)
+    elif action == 'mul':
+        op = "Multiplication"
+        retValue = perform_mul_operation(info)
+    elif action == 'div':
+        op = "Division"
+        retValue = perform_divide_operation(info)
+
+    msg = "Perform '{}' on Python Lists and on NumPy Arrays separately and then Compare their Performance".format(
+        op)
+    print(msg)
+    print("*" * len(msg))
+    print(retValue[0])
+
+    doYouWantToContinue = (input(
+        "Do you want to display values of the Performed Action? Y(es) to display or any key to quit...").strip().upper() or 'Q')[0]
+
+    if doYouWantToContinue == 'Y':
+        print_data(op, info, retValue)
+
+
+def perform_add_operation(info):
+    msg = "Standard Python: \n"
+    startExecution = time.time()
+    perform_Ops = list(
+        map(lambda x, y: (x + y), info['listData'][0], info['listData'][1]))
+    endExecution = time.time()
+    timeTakenForExecution = (endExecution - startExecution)
+    msg += "Total time taken for Standard List Addition: {}\n".format(
+        timeTakenForExecution)
+
+    msg += "\nNumPy: \n"
+
+    startExecution = time.time()
+    perform_Ops_NumPy = np.array(
+        info['listData'][0]) + np.array(info['listData'][1])
+    endExecution = time.time()
+    timeTakenForExecution = (endExecution - startExecution)
+
+    msg += "Total time taken for NumPy Addition: {}\n".format(
+        timeTakenForExecution)
+
+    return [msg, perform_Ops, perform_Ops_NumPy]
+
+
+# Subtraction Operations
+def perform_sub_operation(info):
+    msg = "Standard Python: \n"
+    startExecution = time.time()
+    perform_Ops = list(
+        map(lambda x, y: (y - x), info['listData'][0], info['listData'][1]))
+    endExecution = time.time()
+    timeTakenForExecution = (endExecution - startExecution)
+    msg += "Total time taken for Standard List Subtraction: {}\n".format(
+        timeTakenForExecution)
+
+    msg += "\nNumPy: \n"
+
+    startExecution = time.time()
+    perform_Ops_NumPy = np.array(
+        info['listData'][1]) - np.array(info['listData'][0])
+    endExecution = time.time()
+    timeTakenForExecution = (endExecution - startExecution)
+
+    msg += "Total time taken for NumPy Subtraction: {}\n".format(
+        timeTakenForExecution)
+
+    return [msg, perform_Ops, perform_Ops_NumPy]
+
+
+def perform_mul_operation(info):  # Multiplication Operations
+    msg = "Standard Python: \n"
+    startExecution = time.time()
+    perform_Ops = list(
+        map(lambda x, y: (x * y), info['listData'][0], info['listData'][1]))
+    endExecution = time.time()
+    timeTakenForExecution = (endExecution - startExecution)
+    msg += "Total time taken for Standard List Multiplication: {}\n".format(
+        timeTakenForExecution)
+
+    msg += "\nNumPy: \n"
+
+    startExecution = time.time()
+    perform_Ops_NumPy = np.array(
+        info['listData'][0]) * np.array(info['listData'][1])
+    endExecution = time.time()
+    timeTakenForExecution = (endExecution - startExecution)
+
+    msg += "Total time taken for NumPy Multiplication: {}\n".format(
+        timeTakenForExecution)
+
+    return [msg, perform_Ops, perform_Ops_NumPy]
+
+# Division Operations
+
+
+def perform_divide_operation(info):
+    msg = "Standard Python: \n"
+    startExecution = time.time()
+    perform_Ops = list(
+        map(lambda x, y: (y / x), info['listData'][0], info['listData'][1]))
+    endExecution = time.time()
+    timeTakenForExecution = (endExecution - startExecution)
+    msg += "Total time taken for Standard List Division: {}\n".format(
+        timeTakenForExecution)
+
+    msg += "\nNumPy: \n"
+
+    startExecution = time.time()
+    perform_Ops_NumPy = np.array(
+        info['listData'][1]) / np.array(info['listData'][0])
+    endExecution = time.time()
+    timeTakenForExecution = (endExecution - startExecution)
+
+    msg += "Total time taken for NumPy Division: {}\n".format(
+        timeTakenForExecution)
+
+    return [msg, perform_Ops, perform_Ops_NumPy]
+
+
+def print_data(op, data, returndata):
+    clear_screen()
+    tmp = op + " Results (500 rows per page)\n"
+    op = tmp + "*" * len(tmp)
+    print(op)
+    # print(data)
+    # print(returndata)
---
+++ day-20/python/one-d-array-demo.py
@@ -0,0 +1,128 @@
+# A small program to demostrate all NumPy functionalities
+from helper import *
+
+print("Welcome to NumPy 'Single Dimentional Array' Demo")
+
+continueProgram = "Y"
+
+while continueProgram != "X":
+
+    maxListSize = int(
+        input("Maximum List Size (Min: 25, Max: 10000000): ").strip() or "2500")
+
+    if maxListSize <= 25:
+        maxListSize = 25
+    elif maxListSize >= 10000000:
+        maxListSize = 10000000
+
+    typeOfList = input(
+        "Date Type of list: F for float or any key for Integer: ").strip().upper() or 'I'
+    sequentialOrRandom = input(
+        "Generate List Data: S for sequential or any key for Random: ").strip().upper() or 'R'
+
+    minimum, maximum = 1, 9999998
+
+    if sequentialOrRandom == 'R':
+
+        try:
+            # minimum, maximum = map(int, input(
+            #     "Range of the list (minimum, maximum): ").strip().split(","))
+            minimum, maximum = [int(x) for x in input(
+                "Range of the list (minimum, maximum): ").strip().split(",")[0:2]]
+
+            if minimum <= 0 or maximum <= 0:
+                minimum, maximum = 1, 9999998
+            if minimum >= 9999999:
+                minimum = 1
+            if maximum >= 9999999:
+                maximum = 9999998
+            if minimum >= maximum:
+                minimum, maximum = maximum, minimum
+
+        except:
+            minimum, maximum = 1, 9999998
+
+    # 1-d Python List
+    print("Creating Lists in Python, Pleae wait for a while....")
+    oneDimPythonList = []
+    oneDimPythonList2 = []
+    if typeOfList == 'F' and sequentialOrRandom == 'S':
+        oneDimPythonList = [float(i) for i in range(1, maxListSize)]
+        oneDimPythonList2 = [float(i) for i in range(maxListSize - 1, 0, -1)]
+    elif typeOfList == 'F' and sequentialOrRandom != 'S':
+        oneDimPythonList = [np.random.uniform(
+            minimum, maximum) for i in range(1, maxListSize)]
+        oneDimPythonList2 = [np.random.uniform(
+            minimum, maximum) for i in range(maxListSize - 1, 0, -1)]
+    elif sequentialOrRandom == 'S':
+        oneDimPythonList = [i for i in range(1, maxListSize)]
+        oneDimPythonList2 = [i for i in range(maxListSize - 1, 0, -1)]
+    else:
+        oneDimPythonList = [int(np.random.uniform(
+            minimum, maximum)) for i in range(1, maxListSize)]
+        oneDimPythonList2 = [int(np.random.uniform(
+            minimum, maximum)) for i in range(maxListSize - 1, 0, -1)]
+
+    performOpOnData = 'Y'
+
+    while performOpOnData == 'Y':
+
+        clear_screen()
+        options = "Press\n"
+        options += "(V) View All Informations related to this Array.\n"
+        options += "(L) View Array Data.\n"
+        options += "(A) Add list with itself.\n"
+        options += "(S) Subtract list with itself.\n"
+        options += "(M) Multiply list with itself.\n"
+        options += "(D) Divide list with itself.\n"
+        options += "(X) to Quit to Main Menu.\n"
+
+        interOption = (input(options).strip().upper() or 'V')[0]
+
+        info = {
+            'listSize': maxListSize,
+            'listType': typeOfList,
+            'seqOrRandom': sequentialOrRandom,
+            'ifRandom': (minimum, maximum),
+            'listData': (oneDimPythonList, oneDimPythonList2)
+        }
+
+        if interOption == "V":
+            clear_screen()
+            view_1d_info(info)
+            dummy = input("Press any key to continue...")
+
+        elif interOption == "L":
+            clear_screen()
+            view_1d_array(info)
+            dummy = input("Press any key to continue...")
+
+        elif interOption == "A":
+            clear_screen()
+            perform_operations(info, 'add')
+            dummy = input("Press any key to continue...")
+
+        elif interOption == "S":
+            clear_screen()
+            perform_operations(info, 'sub')
+            dummy = input("Press any key to continue...")
+
+        elif interOption == "M":
+            clear_screen()
+            perform_operations(info, 'mul')
+            dummy = input("Press any key to continue...")
+
+        elif interOption == "D":
+            clear_screen()
+            perform_operations(info, 'div')
+            dummy = input("Press any key to continue...")
+
+        elif interOption == "X":
+            performOpOnData = 'N'
+            clear_screen()
+
+    continueProgram = (input(
+        "Do you want to play with NumPy another time? Press any key to continue or (X) to terminate... ").strip().upper() or 'Y')[0]
+    clear_screen()
+
+# Program End
